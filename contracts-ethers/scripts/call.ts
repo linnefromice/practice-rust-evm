d7996d176ce704d9e94f69de9181ae92b94b0d20
linnefromice <linnefromice@gmail.com>
2024-01-17 12:34:48 +0900

    update (contracts)

--- contracts-ethers/scripts/call.ts
+++ contracts-ethers/scripts/call.ts
@@ -9,7 +9,11 @@ async function main() {
   console.log(`contract address: ${await contract.getAddress()}`)
   console.log(`name: ${await contract.name()}`)
   console.log(`version: ${await contract.version()}`)
+  console.log(`i256_min: ${await contract.get_i256_min()}`)
   console.log(`i256_max: ${await contract.get_i256_max()}`)
+  console.log(`multi i256: ${await contract.get_i256s()}`)
+  console.log(`multi i256 plus: ${await contract.get_i256s_plus()}`)
+  console.log(`multi i256 minus: ${await contract.get_i256s_minus()}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
